dac52ae2e657aa1397ba4fb981dc98fc730741ef
Xuskia11 <atuka.09.123@gmail.com>
2024-12-08 19:16:46 +0400

    commit

--- Codewars6/10.py
+++ Codewars6/10.py
@@ -1,23 +1,10 @@
 def distance(word1, word2):
-    operations = 0
-    num1 = 0
-    num2 = 0
+    word1 = list(word1)
+    for i in word2:
+        if i in word1:
+            word1.remove(i)
+    return len(word1) + 1
 
-    while num1 < len(word1) or num2 < len(word2):
-        if num1 < len(word1) and num2 < len(word2) and word1[num1] == word2[num2]:
-            num1 += 1
-            num2 += 1
-        else:
-            operations += 1
-            if num1 == len(word1):
-                num2 += 1
-            elif num2 == len(word2):
-                num1 += 1
-            else:
-                num1 += 1
-                num2 += 1
-
-    return operations
-
+print(distance("horse", "ros"))
 print(distance("intention", "execution"))
 print(distance("kitten", "sitting"))
